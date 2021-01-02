1e57f3a216ef6070def0d1cbb9418a0de04a9dad
Grazer1823 <dimassentosa77@gmail.com>
2021-01-02 14:48:45 +0700

    basic,type data and operator

---
+++ pertemuan02.py
@@ -0,0 +1,40 @@
+#mencetak kata i love you
+'''jadi kali ini saya akan menunjukan
+operasi sebuah pemrograman'''
+
+#print("I love you")
+
+
+#kata_pertama = "hello world"
+#print(kata_pertama)
+
+#pesan = """halo semua perkenalkan namaku dimas asal sulawesi"""
+#print(pesan)
+
+angka_pertama = 53
+angka_kedua = 4
+# penjumlahan = angka_pertama + angka_kedua
+# print(penjumlahan)
+# print(angka_pertama - angka_kedua)
+# print(angka_pertama * angka_kedua)
+# print(angka_pertama / angka_kedua)
+# print(angka_pertama // angka_kedua)
+# print(type(53/40))
+# print(type(53//40))
+# print(angka_pertama % angka_kedua)
+# print(angka_pertama ** angka_kedua)
+#x = 57 #and
+#print(x > 40 and x <70)
+# x = 57 #for
+# print(x > 40 for x <50)
+# x=13
+# y=10
+# print('x berisi angka',x,'desimal atau', bin(x),'biner')
+# print('y berisi angka',y,'desimal atau', bin(y),'biner')
+# satu=9
+# dua=8
+# logika= satu != dua
+# print(logika)
+# logika2= satu == dua
+# print(logika2)
+print("dimas \n" * 7)
