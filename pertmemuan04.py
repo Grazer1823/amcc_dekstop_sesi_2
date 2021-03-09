a04ae80d7514839ce581358331d84a4198ef6b95
Grazer1823 <dimassentosa77@gmail.com>
2021-03-09 13:53:46 +0700

    github

---
+++ pertmemuan04.py
@@ -0,0 +1,30 @@
+#hallo gaes
+
+
+#input nama dan nim
+#nama = int(input ("Masukan nama Anda = "))
+#nama = input ("Masukan nama Anda = ")
+#konversi_nama = int(nama)
+#nim = input ("Masukan nim Anda = ")
+
+
+#print(nama)
+#print("Perkenalkan nama saya ", nama)
+#print("nim saya adalah ", nim)
+#print("Perkenalkan nama saya ", nama, " berasal dari sulawesi")
+#print("Perkenalkan nama saya %s berasal dari sulawesi." %nama)
+
+#print("data = ", nama, ",type datanya", type(nama))
+
+
+#nama = ()
+#dalam var1abel nama ada yang namanya input
+#didalam input ada bantuan untuk menentukan user untuk mengetahui apa yang dilakukan
+#user input ==> dimas
+#nama = dimas
+
+kata_01 = input("masukan alamat anda = ")
+kata_02 = input("masukan seseorang yang anda benci = ")
+kata_03 = input("kesialan yang sering terjadi = ")
+
+print("desaku {} penuh dengan {} dan setiap hari aku {} .".format(kata_01, kata_02, kata_03))
